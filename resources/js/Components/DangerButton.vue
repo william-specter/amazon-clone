949c461477ac192789a2409078c7b9a3f8c5442d
William-Specter <willyburrel@gmail.com>
2024-11-03 09:33:32 -0500

    Initial Commit

---
+++ resources/js/Components/DangerButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button
+        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-red-600 border border-transparent rounded-md hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:bg-red-700"
+    >
+        <slot />
+    </button>
+</template>
---
+++ resources/js/Components/SecondaryButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        type?: 'button' | 'submit' | 'reset';
+    }>(),
+    {
+        type: 'button',
+    },
+);
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-gray-700 uppercase transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25"
+    >
+        <slot />
+    </button>
+</template>
